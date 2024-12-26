f04e5329cccfe5358817f4cd3f08eccad9eb42a5
up26-dominic-lawrence-bermudez <114312907+up26-dominic-lawrence-bermudez@users.noreply.github.com>
2024-12-27 02:41:35 +0800

    refactor: moved initial positions to common project types

--- python_client/src/project_types.py
+++ python_client/src/project_types.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import Protocol
 
 
 class Player(StrEnum):
@@ -56,3 +57,37 @@ class PieceData:
 
 
 type PlayerPieceData = tuple[Player, PieceData]
+
+
+# todo: does this stay here in project_types?
+# --- MARK: PiecePositions
+
+
+class PiecePositions(Protocol):
+    def get_positions(self) -> dict[Location, tuple[Player, PieceKind]]: ...
+
+
+class BoardGamePiecePositions:
+    def get_positions(self) -> dict[Location, tuple[Player, PieceKind]]:
+        return {
+            # Player 1
+            Location(2, 1): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 2): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 3): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 4): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 5): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 6): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 7): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(2, 8): (Player.PLAYER_1, PieceKind.PAWN),
+            Location(1, 1): (Player.PLAYER_1, PieceKind.KING),
+            # Player 2
+            Location(7, 1): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 2): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 3): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 4): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 5): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 6): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 7): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(7, 8): (Player.PLAYER_2, PieceKind.PAWN),
+            Location(8, 1): (Player.PLAYER_2, PieceKind.KING),
+        }

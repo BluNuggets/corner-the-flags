32b6c6bcc920cc5031181499d095d20a83c5e1b6
up26-dominic-lawrence-bermudez <114312907+up26-dominic-lawrence-bermudez@users.noreply.github.com>
2024-12-26 15:55:14 +0800

    feat (WIP): added boilerplate code for controller

---
+++ python_client/src/main.py
@@ -0,0 +1,27 @@
+# from cs150241project_networking import CS150241ProjectNetworking
+from model import BoardGameModel, Board
+
+
+# todo: rename BoardGameController with actual board game name
+class BoardGameController:
+    _model: BoardGameModel
+    # _view: BoardGameView
+
+    def __init__(self, model: BoardGameModel) -> None:
+        self._model = model
+        # self._view = view
+
+    def start(self) -> None:
+        print("temporary print - controller.start()")
+
+
+def main() -> None:
+    # networking: CS150241ProjectNetworking = CS150241ProjectNetworking.connect("localhost", 15000)
+    model: BoardGameModel = BoardGameModel(Board(8, 8))
+    controller: BoardGameController = BoardGameController(model)
+
+    controller.start()
+
+
+if __name__ == "__main__":
+    main()
